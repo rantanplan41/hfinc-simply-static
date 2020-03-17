692989f0e964bf0887a0403628bc1b084df47300
Daniel Herkenhoff Coelho <dannyboy@MacBook.local>
2020-03-17 01:02:33 -0300

    first commit

---
+++ simply-static-1-1584415730/wp-login.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Holistica Foundation Inc &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script type='text/javascript' src='https://c0.wp.com/c/5.3.2/wp-includes/js/jquery/jquery.js'></script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.3.2/wp-includes/js/jquery/jquery-migrate.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var WFLSVars = {"ajaxurl":"https:\/\/exercise.philanthropy.icu\/wp-admin\/admin-ajax.php","nonce":"7707326cec","recaptchasitekey":"","useCAPTCHA":"","allowremember":"","verification":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://exercise.philanthropy.icu/wp-content/plugins/wordfence/modules/login-security/js/login.1581523568.js?ver=1.0.5'></script>
+<link rel='stylesheet' id='dashicons-css'  href='https://c0.wp.com/c/5.3.2/wp-includes/css/dashicons.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://c0.wp.com/c/5.3.2/wp-includes/css/buttons.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://c0.wp.com/c/5.3.2/wp-admin/css/forms.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://c0.wp.com/c/5.3.2/wp-admin/css/l10n.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://c0.wp.com/c/5.3.2/wp-admin/css/login.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='wordfence-ls-login-css'  href='https://exercise.philanthropy.icu/wp-content/plugins/wordfence/modules/login-security/css/login.1581523568.css?ver=1.0.5' type='text/css' media='all' />
+			<style>
+				.jetpack-sso .message {
+					margin-top: 20px;
+				}
+
+				.jetpack-sso #login .message:first-child,
+				.jetpack-sso #login h1 + .message {
+					margin-top: 0;
+				}
+			</style>
+			<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://i1.wp.com/philanthropy.icu/wp-content/uploads/2017/09/cropped-HFInc_logoFull_square_512-1.jpeg?fit=32%2C32&#038;ssl=1" sizes="32x32" />
+<link rel="icon" href="https://i1.wp.com/philanthropy.icu/wp-content/uploads/2017/09/cropped-HFInc_logoFull_square_512-1.jpeg?fit=192%2C192&#038;ssl=1" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://i1.wp.com/philanthropy.icu/wp-content/uploads/2017/09/cropped-HFInc_logoFull_square_512-1.jpeg?fit=180%2C180&#038;ssl=1" />
+<meta name="msapplication-TileImage" content="https://i1.wp.com/philanthropy.icu/wp-content/uploads/2017/09/cropped-HFInc_logoFull_square_512-1.jpeg?fit=270%2C270&#038;ssl=1" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us jetpack-sso jetpack-sso-form-display">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://exercise.philanthropy.icu/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="https://exercise.philanthropy.icu/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+				<a rel="nofollow" href="https://exercise.philanthropy.icu/wp-login.php?action=register">Register</a> | 					<a href="https://exercise.philanthropy.icu/wp-login.php?action=lostpassword">Lost your password?</a>
+								</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="https://exercise.philanthropy.icu/">
+		&larr; Back to Holistica Foundation Inc		</a></p>
+		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="https://exercise.philanthropy.icu/privacy-policy-cookie-policy/">Privacy Policy &#038; Cookie Policy</a></div>	</div>
+	<link rel='stylesheet' id='jetpack_css-css'  href='https://exercise.philanthropy.icu/wp-content/plugins/jetpack/css/jetpack.css?ver=8.3' type='text/css' media='all' />
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/exercise.philanthropy.icu\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.3.2/wp-includes/js/zxcvbn-async.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.3.2/wp-admin/js/password-strength-meter.min.js'></script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.3.2/wp-includes/js/underscore.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.3.2/wp-includes/js/wp-util.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var userProfileL10n = {"warn":"Your new password has not been saved.","warnWeak":"Confirm use of weak password","show":"Show","hide":"Hide","cancel":"Cancel","ariaShow":"Show password","ariaHide":"Hide password"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.3.2/wp-admin/js/user-profile.min.js'></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
